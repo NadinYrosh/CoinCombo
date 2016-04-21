1bedf88687824676b6257761360e8f553b7b7a5f
John Klein and Nadiya Yeroshkina <nadiya@>
2016-04-21 10:39:15 -0700

    add integration testing, add UI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,43 +1,31 @@
-// import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
-//
-//
-// public class App {
-//   public static void main(String[] args) {
-//     staticFileLocation("/public");
-//     String layout = "templates/layout.vtl";
-//
-//     get("/", (request, response) -> {
-//         HashMap model = new HashMap();
-//         model.put("template", "templates/index.vtl" );
-//         return new ModelAndView(model, "templates/layout.vtl");
-//       }, new VelocityTemplateEngine());
-//
-//     get("/results", (request, response) -> {
-//         HashMap model = new HashMap();
-//         model.put("template", "templates/results.vtl" );
-//
-//         String word1 = request.queryParams("player1");
-//         String word2 = request.queryParams("player2");
-//         Game myGame = new Game();
-//         Boolean winner = myGame.checkWinner(word1, word2);
-//         String outputString = "";
-//
-//         if (winner == null) {
-//           outputString = "It's a tie!";
-//         } else {
-//           if (winner.equals(true)) {
-//             outputString = String.format("Player 1 - %s - WINS!!!", word1);
-//           } else if (winner.equals(false)) {
-//             outputString = String.format("Player 2 - %s - WINS!!!", word2);
-//           }
-//         }
-//
-//         model.put("results", outputString);
-//         return new ModelAndView(model, "templates/layout.vtl");
-//       }, new VelocityTemplateEngine());
-//
-//   }
-// }
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+        HashMap model = new HashMap();
+        model.put("template", "templates/index.vtl" );
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+        HashMap model = new HashMap();
+        model.put("template", "templates/results.vtl" );
+
+        Integer number = Integer.parseInt(request.queryParams("input"));
+        CoinCombo myCombo = new CoinCombo();
+        HashMap<String, Integer> coins = myCombo.comboOfCoins(number);
+        String outputString = String.format("Ypur change is - %d quaters and %d dimes and %d nickes with %d pennies", coins.get("quarters"), coins.get("dimes"), coins.get("nikels"), coins.get("pennies"));
+        model.put("results", outputString);
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+  }
+}
